c865d938d85c5f5c7df7a0257d00ff311476f306
Siddharth kote <siddharthkote128@gmail.com>
2024-12-25 14:58:32 +0530

    Third commit

--- app/src/main/java/HomeView.kt
+++ app/src/main/java/HomeView.kt
@@ -1,18 +1,34 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.notetakingapp.R
 
-val CustomPrimary = Color(0xFF6200EE)
-val CustomOnPrimary = Color(0xFFFFEB3B)
+val CustomPrimary = Color(0xFF87CEEB)
+val CustomOnPrimary = Color.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,15 +36,54 @@ fun HomeView(modifier: Modifier = Modifier) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text(text = "NOTES",style = MaterialTheme.typography.titleLarge,modifier=Modifier.padding(start =24.dp)) },
+                title = { Text(text = "NOTES",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier=Modifier.padding(start =24.dp),
+                    color=Color.Black) },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary,
                     titleContentColor = MaterialTheme.colorScheme.onPrimary
-                )
-            )
+                ),
+                actions = {
+                    Row(modifier = Modifier.padding(start=50.dp),
+                        ) {
+                        IconButton(onClick = {
+                            // Add your exit logic here
+                        },
+                            modifier=Modifier.align) {
+                            Icon(
+                                imageVector = Icons.Default.Menu,
+                                contentDescription = null,
+                                tint = Color.Black,
+
+                            )
+                        }
+                    }
+                })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+
+            },
+                containerColor = MaterialTheme.colorScheme.primary) {
+
+                Text(text = "+", style = MaterialTheme.typography.displayLarge,
+                    color = MaterialTheme.colorScheme.onPrimary)
+            }
         }
+
     ) { paddingValues ->
-        Text(text = "HomeView",modifier = modifier.padding(paddingValues))
+        Box(modifier=Modifier.padding(paddingValues)){
+            Image(
+                painter = painterResource(id= R.drawable.notes_images),
+                contentDescription = "Notes Image",
+                modifier= Modifier
+                    .padding(top = 150.dp, start = 80.dp)
+                    .align(Alignment.Center)
+                    .height(250.dp)
+                    .width(250.dp),
+            contentScale = ContentScale.Crop)
+        }
     }
 }
 
